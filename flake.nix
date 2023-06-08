01ac972c2e4bdb32fb0063430612117001988f21
klarkc <walkerleite490@gmail.com>
2023-06-07 21:51:45 -0300

    feat: add generate

--- flake.nix
+++ flake.nix
@@ -7,14 +7,30 @@
       purs-nix.url = "github:purs-nix/purs-nix";
       nixpkgs.follows = "purs-nix/nixpkgs";
       package-set-repo.url = "github:purenix-org/purenix-package-sets";
+      # FIXME use official-package-set from package-set-repo
+      official-package-set-repo.url = "github:purescript/package-sets";
+      official-package-set-repo.flake = false;
     };
 
   outputs = { self, nixpkgs, utils, package-set-repo, ... }@inputs:
-    utils.apply-systems
+    let
+      __functor = _: { system }:
+        import ./nix/purs-nix inputs nixpkgs.legacyPackages.${system};
+    in
+    { inherit __functor; } // utils.apply-systems
       {
         inherit inputs;
         # TODO remove systems limited by the test
         systems = [ "x86_64-linux" ];
       }
-      ({ system, ... }@ctx: { });
+      ({ system, ... }@ctx:
+        let
+          generator = import ./nix/package-set/generate.nix
+            package-set-repo
+            inputs.official-package-set-repo
+            ctx.pkgs;
+        in
+        {
+          packages.package-set = generator;
+        });
 }
