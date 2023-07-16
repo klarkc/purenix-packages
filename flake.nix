901faaf4b2cf508468691075b73bfd85a1d9b7e5
klarkc <walkerleite490@gmail.com>
2023-07-16 00:04:15 -0300

    build: add nixConfig

--- flake.nix
+++ flake.nix
@@ -92,4 +92,19 @@
           };
         }
       );
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = [
+      "https://klarkc.cachix.org?priority=99"
+      "https://cache.nixos.org"
+    ];
+    extra-trusted-public-keys = [
+      "klarkc.cachix.org-1:R+z+m4Cq0hMgfZ7AQ42WRpGuHJumLLx3k0XhwpNFq9U="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
 }
